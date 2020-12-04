a2be7e8f17aab48e36a18746cd52561d2f2fc5e8
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-12-04 16:30:11 +0100

    Refactor get file content

--- process/cut/cut.py
+++ process/cut/cut.py
@@ -3,14 +3,18 @@ import sys
 flag, delemeter, file = sys.argv[1:]
 
 def cut(flag, delemeter, file):
-    with open(file, 'r') as file_obj:
-        file_content = file_obj.readlines()
-        file_content = [line.rstrip() for line in file_content]
+    file_content = get_file_content(file)
 
     if delemeter[1] == '-':
         print_hyphen_separator(delemeter, file_content)
     elif delemeter[1] == ',':
         print_comma_separator(delemeter, file_content)
+
+def get_file_content(file):
+    with open(file, 'r') as file_obj:
+        file_content = file_obj.readlines()
+    
+    return [line.rstrip() for line in file_content]
 
 def print_hyphen_separator(delemeter, file_content):
     start = int(delemeter[0]) - 1
@@ -24,6 +28,6 @@ def print_comma_separator(delemeter, file_content):
     stop = int(delemeter[2]) - 1
 
     for line in file_content:
-        print(line[start], line[stop])
+        print(line[start], line[stop], sep="")
 
 cut(flag, delemeter, file)
