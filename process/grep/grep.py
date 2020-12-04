f7e1bf268ad654b1c5475d5c23ad8caf26673f63
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-12-04 13:27:25 +0100

    Refactor code

--- process/grep/grep.py
+++ process/grep/grep.py
@@ -20,17 +20,20 @@ parser.add_argument('files', metavar='F', type=str, nargs='+',
 
 args = parser.parse_args()
 
-def strip_new_line(word):
-    return word.rstrip()
+def grep(args):
+    def strip_new_line(word):
+        return word.rstrip()
 
-def check_if_word_exist(word):
-    return args.word in word 
+    def check_if_word_exist(word):
+        return args.word in word 
 
-with open(args.files[0], 'r') as file_obj:
-    content = [strip_new_line(content) for content in file_obj.readlines()]
-    filter_object = [filtered_object for filtered_object in content if check_if_word_exist(filtered_object)]
+    with open(args.files[0], 'r') as file_obj:
+        content = [strip_new_line(content) for content in file_obj.readlines()]
+        filter_object = [filtered_object for filtered_object in content if check_if_word_exist(filtered_object)]
 
-for word in filter_object:
-    print(word)
+    for word in filter_object:
+        print(word)
+
+grep(args)
 
 
