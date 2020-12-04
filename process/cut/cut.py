5c53b3c7a0dd0633d33152e11534471643b1a00d
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-12-04 16:19:16 +0100

    Refactor using fucntions

--- process/cut/cut.py
+++ process/cut/cut.py
@@ -2,21 +2,28 @@ import sys
 
 flag, delemeter, file = sys.argv[1:]
 
-with open(file, 'r') as file_obj:
-    file_content = file_obj.readlines()
-    file_content = [line.rstrip() for line in file_content]
+def cut(flag, delemeter, file):
+    with open(file, 'r') as file_obj:
+        file_content = file_obj.readlines()
+        file_content = [line.rstrip() for line in file_content]
 
-if delemeter[1] == '-':
+    if delemeter[1] == '-':
+        print_hyphen_separator(delemeter, file_content)
+    elif delemeter[1] == ',':
+        print_comma_separator(delemeter, file_content)
+
+def print_hyphen_separator(delemeter, file_content):
     start = int(delemeter[0]) - 1
     end = int(delemeter[2]) 
 
     for line in file_content:
-        print(line[start:end])
-elif delemeter[1] == ',':
+        print(line[start: end])
+ 
+def print_comma_separator(delemeter, file_content):
     start = int(delemeter[0]) - 1
     stop = int(delemeter[2]) - 1
 
     for line in file_content:
-        print(line[start], line[stop], end='')
-        print(end="\n")
+        print(line[start], line[stop])
 
+cut(flag, delemeter, file)
