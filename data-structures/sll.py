d80aa6ecabf4c23dfd22491a68b32a0a9de9055a
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-05-18 12:30:14 +0100

    Implement push in a linked list

--- data-structures/sll.py
+++ data-structures/sll.py
@@ -1,2 +1,27 @@
+class SingleLinkedListNode(object):
+    def __init__(self, value, nxt):
+        self.value = value
+        self.next = nxt
+
+        def __repr__(self):
+            nval = self.next and self.next.value or None
+            return f"[{self.value}:{repr(nval))}]"
+
 class SingleLinkedList(object):
 
+    def __init__(self):
+        self.begin = None
+        self.end = None
+
+    def push(self, obj):
+        node = SingleLinkedListNode(obj, None)
+
+        if self.begin == None:
+            self.begin = node
+            self.end = self.begin
+        else:
+            self.end.next = node
+            self.end = node
+            assert self.begin != self.end
+
+        assert self.end.next == None
